c6e0f8bc2788305e9e9a9bdd034c14ddc9fe4f84
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-05-24 10:01:32 +0200

    print characters list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import { fetchPotter } from './components/Petition';
+import CharacterList from './components/CharacterList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrList: []
+    }
+    this.getCharacters = this.getCharacters.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    fetchPotter()
+      .then(data => {
+        this.setState({
+          arrList: data,
+        })
+      });
+  }
+
+  render() {
+    const { arrList } = this.state;
+    return (
+      <div className="App">
+        <ul className="character_list">
+          {arrList.map(character => {
+            return (
+              <div>
+                <li><img className="character_image" src={character.image} alt=""/></li>
+                <li><h2 className="character_name">{character.name}</h2></li>
+                <li><p className="character_house">{character.house}</p></li>
+              </div>
+            )
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CharacterList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CharacterList extends React.Component {
+    
+    render() {
+        
+        return (
+            <React.Fragment>
+                
+            </React.Fragment>
+        )
+    }
+}
+
+export default CharacterList;
